a9c1ae215dc499c5a4ea1e03aa16cf44ce60eeb1
Lucas Martire <marlumon125@gmail.com>
2022-07-07 18:12:10 -0300

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+const HelloWorld = ()=>{
+	return <h2>Hello World<h2></h2>;
+};
+
+export default HelloWorld;
+
+
+
+/*
+const NavBar = ({name})=>{
+	return(
+		<nav>
+			<p>{name}</p>
+			<p>Home</p>
+			<p>Productos</p>
+			<p>Detalles</p>
+		</nav>
+	);
+};
+
+export default NavBar;*/
